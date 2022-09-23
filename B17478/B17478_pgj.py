f18f15b57ba37e6f0a5f9f702eda11a12e5edc0a
경준 <kj0284@naver.com>
2022-09-23 19:27:11 +0900

    B17478_pgj_01

---
+++ B17478/B17478_pgj.py
@@ -0,0 +1,30 @@
+def repeat(a, cnt):
+    string_arr =["\"재귀함수가 뭔가요?\"","\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.",
+                "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.",
+                "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\""]
+    string_arr1 = ["\"재귀함수가 뭔가요?\"", "\"재귀함수는 자기 자신을 호출하는 함수라네\""]
+    if(cnt == a):
+        for i in range(len(string_arr1)):
+            for j in range(cnt):
+                print("____", end="")
+            print(string_arr1[i])
+        cnt += 1
+
+
+    else:
+        for i in range(len(string_arr)):
+            for j in range(cnt):
+                print("____", end="")
+            print(string_arr[i])
+        cnt+= 1
+        repeat(a, cnt)    
+    for i in range(cnt-1):
+        print("____", end="")
+    print("라고 답변하였지.")
+
+
+a = int(input())
+print("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.")
+count = 0
+repeat(a, count)
+
