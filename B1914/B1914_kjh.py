96a238bbae4d2d7124a377f543903c5265303426
Jaehyeon <jaehyeonkim2358@gmail.com>
2022-09-23 22:51:24 +0900

    N-Queen 30840kb 20272ms

--- B1914/B1914_kjh.py
+++ B1914/B1914_kjh.py
@@ -5,9 +5,11 @@ path_list = []
 def hanoi(start, mid, dest, depth):
     if depth == n:
         return
+    # 출발 지점에서 중간 지점으로
     hanoi(start, dest, mid, depth+1)
     global path_list
     path_list.append(f'{start} {dest}')
+    # 중간 지점에서 목적지로
     hanoi(mid, start, dest, depth+1)
 
 print(2**n -1)
