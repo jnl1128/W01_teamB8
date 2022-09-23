660d25389055464087535e4e93b24985ac1f878f
narin <nalynnshinhwa1128@gmail.com>
2022-09-23 22:05:47 +0900

    0923_22시 커밋

---
+++ B10819/B10819_jnl.py
@@ -0,0 +1,37 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split(' ')))
+
+def splice(arr, idx):
+    return arr[:idx]+arr[idx+1:len(arr)]
+
+def diffSum(arr):
+    total = 0
+    for i in range(len(arr)-1):
+        total += abs(arr[i+1] - arr[i])
+    return total
+    
+def permutation(lst):
+    if len(lst) == 0:
+        return []
+    
+    if len(lst) == 1:
+        return [lst]
+    
+    l = []
+
+    for i in range(len(lst)):
+        m = lst[i]
+
+        for p in permutation(splice(lst, i)):
+            l.append([m]+p)
+
+    return l
+
+maxV = 0
+
+for p in permutation(A):
+    maxV = max(maxV, diffSum(p))
+print(maxV)
---
+++ B2798/B2798_jnl.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split(' '))
+cards = list(map(int, input().split(' ')))
+cards.sort()
+
+result = 0
+
+for i in range(len(cards)-2):
+    for j in range(i+1, len(cards)):
+        for k in range(j+1, len(cards)):
+            if (cards[i]+cards[j]+cards[k]) <= M:
+                result = max(result,cards[i]+cards[j]+cards[k])
+print(result)
