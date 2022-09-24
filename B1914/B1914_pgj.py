5710a36a32ec83930b743ac6b24a730752f83b4e
경준 <kj0284@naver.com>
2022-09-24 16:41:01 +0900

    B1914_pgj_01

---
+++ B1914/B1914_pgj.py
@@ -0,0 +1,12 @@
+def hanoi(n, start, end, aux): 
+#n : 반복할 횟수, start: 원반의 시작점, end, 원반의 도착점, aux : 원반의 경유지
+    if(n == 1):
+        print(start, end)
+        return
+    hanoi(n-1, start, aux, end)
+    hanoi(1, start,end,aux)
+    hanoi(n-1, aux, end, start)
+
+n = int(input())
+print(2**n-1)
+hanoi(n,1,3,2)
