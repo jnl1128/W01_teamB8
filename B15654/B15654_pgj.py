489e32e29e2554179f0609ba4773d2bb45cf5787
경준 <kj0284@naver.com>
2022-09-28 21:10:25 +0900

    B15654_pgj_01

---
+++ B15654/B15654_pgj.py
@@ -0,0 +1,26 @@
+from copy import deepcopy
+import sys
+
+def dfs(arr, n):
+    result_arr = []
+
+
+    if n == 1:
+        for i in arr:
+            result_arr.append([i])
+    elif n > 1:
+        for i in range(len(arr)):
+            ans = deepcopy(arr)
+            ans.pop(i)
+            for j in dfs(ans, n-1):
+                result_arr.append([arr[i]]+j)
+    return result_arr
+
+num, repeat_num = map(int, sys.stdin.readline().strip().split())
+
+arr_num = list(map(int,sys.stdin.readline().strip().split()))
+arr_num.sort()
+result_arr = dfs(arr_num, repeat_num)
+
+for elem in result_arr:
+    print(*elem, sep=' ')
