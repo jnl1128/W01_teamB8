5160a9e7d29cf4c0e62b4d5c5b1302c121402b98
Jaehyeon <jaehyeonkim2358@gmail.com>
2022-09-28 17:00:34 +0900

    B15656 30840kb 76ms

--- B15656/B15656_kjh.py
+++ B15656/B15656_kjh.py
@@ -2,11 +2,11 @@ import sys
 n, m = map(int, sys.stdin.readline().split())
 nums = sorted(list(map(int, sys.stdin.readline().split())))
 
-def solution(depth, result):
+def solution(depth, pre, result):
     if depth == m:
         sys.stdout.write(result+"\n")
         return
-    for i in range(n):
-        solution(depth+1, result + str(nums[i])+ ' ')
+    for i in range(pre, n):
+        solution(depth+1, i, result + str(nums[i])+ ' ')
 
-solution(0, '')
+solution(0, 0, '')
