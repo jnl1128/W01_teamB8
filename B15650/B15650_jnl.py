3af75701a411b0843f56856761e5d01c19ca726e
narin <nalynnshinhwa1128@gmail.com>
2022-09-28 22:11:52 +0900

    N과M(2)_시간보완 30840kb 68ms

--- B15650/B15650_jnl.py
+++ B15650/B15650_jnl.py
@@ -6,21 +6,16 @@ N, M = map(int, input().split(' '))
 arr = [i for i in range(1, N+1)]
 
 visited = [0 for _ in range(N+1)]
-result_lst = []
 
-def solution(depth, stack):
+def solution(depth, stack, start):
     if depth == M:
-        stack.sort()
-        if stack not in result_lst:
-            result_lst.append(stack)
+        print(*stack)
         return
     
-    for i in range(1, N+1):
+    for i in range(start, N+1):
         if visited[i] == 0:
             visited[i] = 1
-            solution(depth+1, stack+[arr[i-1]])
+            solution(depth+1, stack+[arr[i-1]], i)
             visited[i] = 0
 
-solution(0, [])  
-for elem in result_lst:
-    print(*elem, sep=' ')
+solution(0, [], 1)  
